5cd15979344047741dd78d1b18c38dd922c8e0ac
RickBarretto <78623871+RickBarretto@users.noreply.github.com>
2024-11-30 12:45:26 -0300

    match the Evaluation's result with the Weigths' input

--- src/web/schemas/poem.py
+++ src/web/schemas/poem.py
@@ -13,36 +13,30 @@ class IsPoem(Protocol):
     @property
     def evaluation(self) -> DomainEvaluation: ...
 
-class RhymeScore(BaseModel):
-    structure: float
-    internal: float
-    consonant: float
-
 
 class EvaluationScore(BaseModel):
-    accent: float
-    stress: float
-    rhymes: RhymeScore
-    result: float
+    vocal_harmony: float
+    accentuation: float
+    tonic_position: float
+    internal_rhyme: float
+    rhythmic_structure: float
 
 
 class Evaluation(BaseModel):
     scores: EvaluationScore
+    score: float
 
     @staticmethod
-    def from_domain(evaluation: DomainEvaluation) -> "Evaluation":
+    def from_domain(model: DomainEvaluation) -> "Evaluation":
         schema_score = EvaluationScore(
-            accent=evaluation.accent_score,
-            stress=evaluation.stress_score,
-            result=evaluation.score_result,
-            rhymes=RhymeScore(
-                structure=evaluation.rhyme_structure_score,
-                internal=evaluation.intern_rhyme_score,
-                consonant=evaluation.consonant_rhyme_score,
-            ),
+            accentuation=model.accent_score,
+            internal_rhyme=model.intern_rhyme_score,
+            rhythmic_structure=model.rhyme_structure_score,
+            tonic_position=model.stress_score,
+            vocal_harmony=model.consonant_rhyme_score
         )
 
-        return Evaluation(scores=schema_score)
+        return Evaluation(scores=schema_score, score=model.score_result)
 
 
 class Poem(BaseModel):
